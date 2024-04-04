e5c453eaeffad97249762d593765bdc7061586fb
le-huynh <lehuynhtrucly.hcmus@gmail.com>
2024-04-04 16:55:11 +0900

    testing

---
+++ r.R
@@ -0,0 +1,10 @@
+# test #------
+`r text = c("#---\n",
+            "# This is my awesome snippet.\n",
+            "#\n",
+            "# Note: Use the TAB key for indenting the snippet's content instead of spaces.\n",
+            "#\n",
+            "# Cheers!\n",
+            "#---\n")
+
+paste(text, collapse = "")`
